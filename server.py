305ad4361401b919df917a5152a128c257eb5b80
Brahmia Lokman Abdelmoname <brahmia.lokmeneabdelmoname@univ-guelma.dz>
2025-11-21 10:19:31 -0500

    add se3rver file

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask ,request , render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotion_detect() :
+    text_analyzer = request.args.get("textToAnalyze")
+    response = emotion_detector(text_analyzer)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response["sadness"]
+    dominant_emotion = response['dominant_emotion']
+    return f"For the given statement, the system response is 'anger' : {anger} , 'disgust' : {disgust}, 'fear' : {fear}, 'joy' : {joy}, 'sadness' : {sadness} .\n The dominant emotion is {dominant_emotion} . "
+
+@app.route("/")
+def render_index_page() :
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
